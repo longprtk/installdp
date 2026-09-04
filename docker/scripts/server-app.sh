bb886d2de2fff2d00cbffadedfd997fd0b732ae0
Herryhuynh <58300258+longprtk@users.noreply.github.com>
2026-09-04 20:34:38 +0700

    Update server-app.sh

--- docker/scripts/server-app.sh
+++ docker/scripts/server-app.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-REPO_URL="https://github.com/vulebaolong/devops_04.git"
+REPO_URL="https://github.com/longprtk/devops_04.git"
 REPO_DIR="$HOME/devops_04"
 COMPOSE_DIR="$REPO_DIR/docker-compose"
 
